8fea6d54fdc1e9b0621ccaf3fd138a9e6fb07f34
QuackQuackBOOM <34162376+QuackQuackBOOM@users.noreply.github.com>
2017-12-04 15:05:29 +0000

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -10,10 +10,10 @@ import lejos.robotics.subsumption.Behavior;
 public class MainClass {
 	public static final int NUM_OF_SORTS = 1;
 	public static final int NUM_OF_BLOCKS = 3;
-	public static final int MAX_SPEED = 100;
+	public static final int MAX_SPEED = 75;
 	public static final double WHEEL_DIAMETER = 55;
-	public static final double AXEL_LENGTH = 138;
-	public static final int GRAB_DIST = 100;
+	public static final double AXEL_LENGTH = 137.75;
+	public static final int GRAB_DIST = 200;
 	
 	public static LightSensor ls = new LightSensor(SensorPort.S1);
 	public static DifferentialPilot pilot = new DifferentialPilot(MainClass.WHEEL_DIAMETER, MainClass.AXEL_LENGTH, Motor.B, Motor.A);
@@ -29,7 +29,7 @@ public class MainClass {
 	
 	public static MovStack movementStack = new MovStack();
 	
-	public static int[] Values = {1,2,3};
+	public static int[] Values = {3,2,1};
 	
 	public static void main(String[] args) {
 		//Calabrate Light Sensor
@@ -39,8 +39,9 @@ public class MainClass {
 			Button.ENTER.waitForPressAndRelease();
 		} catch(Exception e){}
 		
-		pilot.setRotateSpeed(50);
+		pilot.setRotateSpeed(250);
 		pilot.setTravelSpeed(50);
+		Motor.C.setSpeed(50);
 		
 		//##change claw to start pos
 		
@@ -102,7 +103,8 @@ public class MainClass {
 	public static void setCurrentPos(int newPos) {
 		if (newPos != currentPos) {
 			currentPos = newPos;
-			LCD.drawInt(currentPos, 5, 0);
+			LCD.clear(0);
+			LCD.drawString("Cpos:"+currentPos, 0, 0);
 			LCD.refresh();
 		}
 	}
@@ -113,7 +115,8 @@ public class MainClass {
 	public static void setNextPos(int newPos) {
 		if (newPos != nextPos) {
 			nextPos = newPos;
-			LCD.drawInt(nextPos, 5, 1);
+			LCD.clear(1);
+			LCD.drawString("Npos:"+nextPos, 0, 1);
 			LCD.refresh();
 		}
 	}
